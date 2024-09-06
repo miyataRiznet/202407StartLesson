ea4f2632732fc7e1616b8e075da17bc89c1239d6
okamoto <okamoto.takahiro@riz-net.jp>
2024-09-06 11:23:04 +0900

    lesson02.pyのコメントを削除

--- okamoto/リスト課題/lesson02.py
+++ okamoto/リスト課題/lesson02.py
@@ -8,7 +8,6 @@ import random
 members = ['大迫', '古橋', '久保', '堂安', '南野', '伊東', '田中', '遠藤', '富安', '吉田', '長友', '酒井']
 random.shuffle(members)  # 配列の中身をシャッフル
 
-<<<<<<< HEAD
 # チームAのリストを作成
 team_a = [members.pop(i) for i in range(int(len(members) / 2))]
 
@@ -16,14 +15,3 @@ team_a = [members.pop(i) for i in range(int(len(members) / 2))]
 team_a_leader, team_b_leader = random.choice(team_a), random.choice(members)
 
 print(f'チームA{team_a}リーダーは{team_a_leader}\nチームB{members}リーダーは{team_b_leader}')
-=======
-# チームAとチームBのリストを作成
-team_a = [members.pop(i) for i in range(int(len(members) / 2))]
-team_b = members
-
-# それぞれのチームからランダムなメンバーを選択し、リーダーとする
-team_a_leader, team_b_leader = random.choice(team_a), random.choice(team_b)
-
-print(f'チームA{team_a}リーダーは{team_a_leader}\nチームB{team_b}リーダーは{team_b_leader}')
->>>>>>> origin/okamoto
-# git test
