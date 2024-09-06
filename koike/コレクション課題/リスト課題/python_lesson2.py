7dd3273885f069fe376175b72f73716873db659b
koike <koike.wataru@riz-net.jp>
2024-09-06 11:53:13 +0900

    修正できてるかテスト

--- koike/コレクション課題/リスト課題/python_lesson2.py
+++ koike/コレクション課題/リスト課題/python_lesson2.py
@@ -1,5 +1,6 @@
 import random
 
-members = ['大迫','古橋','久保','堂安','南野','伊東','田中','遠藤','富安','吉田','長友,'酒井']
-         
-random.sample(members, 6)
+members = ['大迫','古橋','久保','堂安','南野','伊東','田中','遠藤','富安','吉田','長友','酒井']
+random.shuffle(members) #配列をシャッフル
+
+print(random.sample(6))
