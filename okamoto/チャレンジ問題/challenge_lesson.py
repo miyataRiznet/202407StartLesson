cbc2fe2b4031f14b919a8e6d1d926c2ca672d25b
okamoto <okamoto.takahiro@riz-net.jp>
2024-09-09 12:07:20 +0900

    修正のため

--- okamoto/チャレンジ問題/challenge_lesson.py
+++ okamoto/チャレンジ問題/challenge_lesson.py
@@ -32,7 +32,8 @@ game_times = player_num - 1                                                   # 
 total_card = 0
 player_list = [i + 1 for i in range(player_num)]
 
-for i in range(game_times, 0, -1):
+print(game_times)
+for i in range(game_times + 1, 1, -1):
     total_card += trump_game(i)
 
 print(f'優勝賞金{total_card}万円')
