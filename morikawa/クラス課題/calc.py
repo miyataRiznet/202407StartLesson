f96728183182fd418b1310c0bee1c61e4b702ec6
morikawa <morikawa.koji@riz-net.jp>
2024-09-10 14:32:01 +0900

    クラス課題2提出

---
+++ morikawa/クラス課題/calc.py
@@ -0,0 +1,8 @@
+## クラス課題2用の、Calculationクラスが配置されたファイル
+
+class Calculation:
+    def add(self, num1, num2):
+        return num1 + num2
+    
+    def sub(self, num1, num2):
+        return num1 - num2
